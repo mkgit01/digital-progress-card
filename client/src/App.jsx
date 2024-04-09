7c7dd7f2424e68355ce195ad3ce738880d061952
devankitmishra <ankitmishrapuri@gmail.com>
2024-04-09 17:47:27 +0530

    Committing changes to App.jsx before merging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,15 @@ import CreateTask from './components/CreateTask'
 
 import './styles/index.css'
 import Header from './components/Header'
+import Profile from './components/Profile'
 
 function App() {
   return (
     <>
       <Header />
-      <Dashboard />
+      {/* <Dashboard /> */}
       {/* <CreateTask /> */}
+      <Profile/>
       <Footer />
     </>
   )
