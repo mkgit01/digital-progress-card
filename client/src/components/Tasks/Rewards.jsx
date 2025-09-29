6ebe4a65d72847af8162d6c628f5795dccb286ab
Ankit Mishra <ankitmishrapuri@gmail.com>
2025-09-29 10:52:19 +0530

    fix: api url fixed

--- client/src/components/Tasks/Rewards.jsx
+++ client/src/components/Tasks/Rewards.jsx
@@ -46,7 +46,7 @@ export default function Rewards() {
     const fetchRewards = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:5000/api/rewards/${user.uid}`
+          `https://digital-progress-card.onrender.com/api/rewards/${user.uid}`
         );
 
         const tasksData = response.data.map((task) => {
